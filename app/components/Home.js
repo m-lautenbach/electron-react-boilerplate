b26859d2e7abf31d0a8b6c0a1cca527e2c98ce77
Markus Lautenbach <m.lautenbach@xpure.de>
2016-05-31 22:41:30 +0200

    Revert manual saga test

--- app/components/Home.js
+++ app/components/Home.js
@@ -5,15 +5,14 @@ import styles from './Home.css';
 export default class Home extends Component {
   static propTypes = {
     login: PropTypes.func.isRequired,
-    requestAccessToken: PropTypes.func.isRequired
   };
   render() {
-    const { login, requestAccessToken } = this.props;
+    const { login } = this.props;
     return (
       <div>
         <div className={styles.container}>
           <h2>Figo-Client</h2>
-          <button className={styles.btn} onClick={requestAccessToken}>Login</button>
+          <button className={styles.btn} onClick={login}>Login</button>
         </div>
       </div>
     );
