e90908c8ca8c102b0d5bdfd7368c55a38f3707f3
Stephan Schubert <stephan@frozencherry.de>
2016-05-31 17:44:58 +0200

    Update `Transaction` component

--- app/components/Transaction.jsx
+++ app/components/Transaction.jsx
@@ -1,19 +1,24 @@
 import React, { Component, PropTypes } from 'react';
 import { Link } from 'react-router';
 import mapProps from 'map-props'
+import classNames from 'classnames'
+import moment from 'moment'
+
 import styles from './Transaction.css';
 
 @mapProps({
-  amount: props => `${props.amount} ${props.currency}`
+  withdrawal: props => props.amount < 0,
+  amount: props => (props.amount < 0 ? '⊖' : '⊕') + `${Math.abs(props.amount)} ${props.currency}`,
+  bookingDate: props => moment(props.bookingDate).format('DD.MM.YYYY')
 })
 class Transaction extends Component {
   static propTypes = {
     accountNumber: PropTypes.string.isRequired,
-    amount: PropTypes.string.isRequired,
+    amount: PropTypes.number.isRequired,
     bankCode: PropTypes.string.isRequired,
     bankName: PropTypes.string.isRequired,
     booked: PropTypes.bool,
-    bookingDate: PropTypes.string,
+    //bookingDate: PropTypes.string,
     bookingText: PropTypes.string,
     currency: PropTypes.string.isRequired,
     name: PropTypes.string.isRequired,
@@ -30,39 +35,45 @@ class Transaction extends Component {
       booked,
       bookingDate,
       bookingText,
+      currency,
       name,
       purpose,
       valueDate,
+      withdrawal
     } = this.props;
 
-    var bookInfo;
+    var metaInfo = <ol className={styles.metaInfo}>
+      <li className={styles.accountNumber}>
+        <h6>Kontonummer</h6>
+        {accountNumber}
+      </li>
+      <li className={styles.bankCode}>
+        <h6>Bankleitzahl</h6>
+        {bankCode}
+      </li>
+      <li className={styles.bankName}>
+        <h6>Name der Bank</h6>
+        {bankName}
+      </li>
+      <li className={styles.bookingDate}>
+        <h6>Buchungsdatum</h6>
+        {bookingDate}
+      </li>
+    </ol>;
 
-    if (booked) {
-      bookInfo = <dl className={styles.bookInfo}>
-        <dt>Buchungsdatum</dt>
-        <dd>{bookingDate}</dd>
-        <dt>Buchungstext</dt>
-        <dd>{bookingText}</dd>
-      </dl>
-    }
+    var css = classNames(styles.container, {
+      [styles.withdrawal]: withdrawal
+    });
 
     return (
-      <div className={styles.transaction}>
-        <div className="main">
-          <div className={styles.name}>{name}</div>
-          <div className={styles.purpose}>{purpose}</div>
-          <div className={styles.amount}>{amount}</div>
-        </div>
-        <dl>
-          <dt className={styles.accountNumber}>Kontonummer</dt>
-          <dd className={styles.accountNumber}>{accountNumber}</dd>
-          <dt className={styles.bankCode}>Bankleitzahl</dt>
-          <dd className={styles.bankCode}>{bankCode}</dd>
-          <dt className={styles.bankName}>Name der Bank</dt>
-          <dd className={styles.bankName}>{bankName}</dd>
-        </dl>
-        {bookInfo}
+    <div className={css}>
+      <div className={styles.main}>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.purpose}>{purpose}</div>
+        <div className={styles.amount}>{amount}</div>
       </div>
+      {metaInfo}
+    </div>
     );
   }
 }
