0eb451f89dbc64865b6b6a17c6725a1ad1787329
Markus Lautenbach <m.lautenbach@xpure.de>
2016-05-31 22:49:12 +0200

    Use existing action creator to start session with access token

--- app/sagas.js
+++ app/sagas.js
@@ -3,14 +3,14 @@ import { takeEvery } from 'redux-saga';
 import { call, put } from 'redux-saga/effects';
 
 import figoApi from './api/figo';
-
+import {startSession} from './actions/user'
 
 function* fetchAccessToken(action) {
     try {
         var client_id, client_secret, username, password;
         ({client_id, client_secret, username, password} = action.payload);
         const token = yield figoApi.aquireAccessToken(client_id, client_secret, username, password);
-        yield put({type: 'ACCESS_TOKEN_FETCH_SUCCEEDED', token: token});
+        yield put(startSession(token.access_token));
     } catch (ex) {
         yield put({type: 'ACCESS_TOKEN_FETCH_FAILED', message: ex.message});
     }
