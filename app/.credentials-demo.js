2bfd8c00fec9356e4a5afe1d0982835d66081ad5
Markus Lautenbach <m.lautenbach@xpure.de>
2016-05-31 22:08:10 +0200

    Add demo credentials for testing and as example

---
+++ app/.credentials-demo.js
@@ -0,0 +1,6 @@
+export default {
+    client_id: "CygnIYss8101KTkm1d0WgO4gFn4yvwWESKWfrVhhTkvE",
+    client_secret: "STsRqwSvVuZNoeV-vAZvLf8Zc3-qETEX9xAeb15qz5Oc",
+    username: 'demo@figo.me',
+    password: 'demo1234'
+}
